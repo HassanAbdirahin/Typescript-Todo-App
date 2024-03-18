42de4d29c8568b7f8b6c0e5513adbdd6e5f1cc9b
HassanAbdirahin <hassanabdirahin441@gmail.com>
2024-03-18 10:00:46 +0300

    initial commit
    made the first creations to the app

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,67 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { v4 as uuidV4 } from "uuid";
+console.log(uuidV4());
 
-import confetti from 'canvas-confetti';
+type Task = {
+  id: string;
+  title: string;
+  isComplete: boolean;
+  createdAt: Date;
+};
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+const list = document.querySelector<HTMLUListElement>("#list");
+const form = document.getElementById("todo-form") as HTMLFormElement | null;
+const input = document.querySelector<HTMLInputElement>("#todo-title");
+
+const tasks: Task[] = loadTasks();
+
+tasks.forEach(addListItem);
+
+form?.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (!input?.value) {
+    return;
+  }
+
+  const newTask: Task = {
+    id: uuidV4(),
+    title: input.value,
+    isComplete: false,
+    createdAt: new Date(),
+  };
+
+  // add task to arr
+  tasks.push(newTask);
+
+  addListItem(newTask);
+
+  // save to Local Storage
+  saveTasks();
+
+  input.value = "";
+});
+
+function addListItem(task: Task): void {
+  const item = document.createElement("li");
+  const label = document.createElement("label");
+  const checkbox = document.createElement("input");
+  checkbox.addEventListener("change", () => {
+    task.isComplete = !task.isComplete;
+    console.log(tasks);
+  });
+  checkbox.type = "checkbox";
+  checkbox.checked = task.isComplete;
+  label.append(checkbox, task.title);
+  item.append(label);
+  list?.append(item);
+}
+
+function saveTasks() {
+  localStorage.setItem("TASKS", JSON.stringify(tasks));
+}
+
+function loadTasks(): Task[] {
+  const taskJSON = localStorage.getItem("TASKS");
+  if (taskJSON === null) return [];
+  return JSON.parse(taskJSON);
+}
